78827c3200f06284824cd3c8f6f69029299551d2
mooiman <jan.mooiman@deltares.nl>
2019-11-09 22:12:43 +0100

    Adjusted batch because layout of directory structure is changed

--- dispatch_plotcfts.bat
+++ dispatch_plotcfts.bat
@@ -1,11 +1,11 @@
 rem copy to local d:\bin
-copy .\release\plot_cf_time_series.exe  d:\bin\plotcfts\plotcfts.exe
-copy .\release\*.dll                    d:\bin\plotcfts\*.*
-copy .\release\platforms\*.dll          d:\bin\plotcfts\platforms\*.*
-copy .\doc\plotcfts_um.pdf              d:\bin\plotcfts\doc\plotcfts_um.pdf
+copy .\bin\x64\release\plot_cf_time_series.exe  d:\bin\plotcfts\plotcfts.exe
+copy .\bin\x64\release\*.dll                    d:\bin\plotcfts\*.*
+copy .\bin\x64\release\platforms\*.dll          d:\bin\plotcfts\platforms\*.*
+copy .\doc\plotcfts_um.pdf                      d:\bin\plotcfts\doc\plotcfts_um.pdf
 
 rem copy to Bulletin
-copy .\release\plot_cf_time_series.exe  n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\plotcfts.exe
-copy .\release\*.dll                    n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\*.*
-copy .\release\platforms\*.dll          n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\platforms\*.*
-copy .\doc\plotcfts_um.pdf              n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\doc\plotcfts_um.pdf 
+copy .\bin\x64\release\plot_cf_time_series.exe  n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\plotcfts.exe
+copy .\bin\x64\release\*.dll                    n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\*.*
+copy .\bin\x64\release\platforms\*.dll          n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\platforms\*.*
+copy .\doc\plotcfts_um.pdf                      n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\doc\plotcfts_um.pdf 
