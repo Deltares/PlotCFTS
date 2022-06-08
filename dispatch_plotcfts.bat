3d5afd933cb298541bfca1009f2c757ed58753e4
Jan Mooiman <jan.mooiman@deltares.nl>
2022-06-08 20:30:40 +0200

    Update release notes in documentation

--- dispatch_plotcfts.bat
+++ dispatch_plotcfts.bat
@@ -11,7 +11,7 @@ copy .\bin\x64\release\platforms\*.dll          "c:\Program Files\Deltares\plotc
 copy .\doc\plotcfts_um.pdf                      "c:\Program Files\Deltares\plotcfts\doc\plotcfts_um.pdf"
 
 rem copy to Bulletin
-copy .\bin\x64\release\plotcfts.exe             n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\plotcfts.exe
-copy .\bin\x64\release\*.dll                    n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\*.*
-copy .\bin\x64\release\platforms\*.dll          n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\platforms\*.*
-copy .\doc\plotcfts_um.pdf                      n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS\doc\plotcfts_um.pdf 
+copy .\bin\x64\release\plotcfts.exe             n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_3.07.01\plotcfts.exe
+copy .\bin\x64\release\*.dll                    n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_3.07.01\*.*
+copy .\bin\x64\release\platforms\*.dll          n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_3.07.01\platforms\*.*
+copy .\doc\plotcfts_um.pdf                      n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_3.07.01\doc\plotcfts_um.pdf 
