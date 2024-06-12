457ca1cc842072d95a7b0cf7ade23c6c617d84fa
Jan Mooiman <jan.mooiman@deltares.nl>
2024-06-12 23:42:13 +0200

    Update QT libraries to 6.5.2

--- dispatch_plotcfts.bat
+++ dispatch_plotcfts.bat
@@ -1,11 +1,11 @@
-rem copy to local  "c:\Program Files\Deltares"  needed for QGIS
+arem copy to local  "c:\Program Files\Deltares"  needed for QGIS
 copy .\bin\x64\release\plotcfts.exe             "c:\Program Files\Deltares\plotcfts\plotcfts.exe"
 copy .\bin\x64\release\*.dll                    "c:\Program Files\Deltares\plotcfts\*.*"
 copy .\bin\x64\release\platforms\*.dll          "c:\Program Files\Deltares\plotcfts\platforms\*.*"
 copy .\doc\plotcfts_um.pdf                      "c:\Program Files\Deltares\plotcfts\doc\plotcfts_um.pdf"
 
 rem copy to Bulletin
-copy .\bin\x64\release\plotcfts.exe             n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_4.04.00\plotcfts.exe
-copy .\bin\x64\release\*.dll                    n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_4.04.00\*.*
-copy .\bin\x64\release\platforms\*.dll          n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_4.04.00\platforms\*.*
-copy .\doc\plotcfts_um.pdf                      n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_4.04.00\doc\plotcfts_um.pdf 
+copy .\bin\x64\release\plotcfts.exe             n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_4.04.01\plotcfts.exe
+copy .\bin\x64\release\*.dll                    n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_4.04.01\*.*
+copy .\bin\x64\release\platforms\*.dll          n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_4.04.01\platforms\*.*
+copy .\doc\plotcfts_um.pdf                      n:\Deltabox\Bulletin\mooiman\programs\PlotCFTS_4.04.01\doc\plotcfts_um.pdf 
