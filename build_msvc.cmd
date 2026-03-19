b0f3be7f9d21d39d6a23aa2ac76986dc67be981e
Jan Mooiman <jan.mooiman@outlook.com>
2026-03-19 17:52:08 +0100

    Update CMAKE for MSVC

--- build_msvc.cmd
+++ build_msvc.cmd
@@ -6,13 +6,13 @@ set PATH=c:\Qt\Qt6.10.2\6.10.2\msvc2022_64\;%PATH%
 set PATH=c:\boost\Boost-1.90.0\;%PATH%
 
 del %exec%
-del _build
+del /Q _build
 
 copy packages\include\plot_cf_time_series_version.h.vcs packages\include\plot_cf_time_series_version.h 
 copy packages\include\plot_cf_time_series_version.rc.vcs packages\include\plot_cf_time_series_version.rc 
 
 echo on
 cmake -B _build > cmake_configure.log 2>&1
-cmake --build _build --verbose > cmake_build.log 2>&1
+cmake --build _build --verbose --config Release > cmake_build.log 2>&1
 
 %exec%
